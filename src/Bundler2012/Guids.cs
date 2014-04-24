24a5416777ac11eefa70e0a7a4c1f1ab0d4f7a29
Vlad Ogay <ogay.vlad@gmail.com>
2014-04-24 18:17:14 +0400

    Source code

---
+++ src/Bundler2012/Guids.cs
@@ -0,0 +1,16 @@
+﻿// Guids.cs
+// MUST match guids.h
+
+using System;
+
+namespace Bundler2012
+{
+    static class GuidList
+    {
+        public const string guidBundlerRunOnSavePkgString = "68CE1BF0-D4F8-4F45-BB02-F7C51995F121";
+        public const string guidBundlerRunOnSaveCmdSetString = "339CEF60-F5ED-4E75-8A7A-18A153C2EB47";
+        public const string guidBundlerRunOnSaveOutputWindowPane = "324112CA-3C02-451A-9E53-A9C66A31E833";
+
+        public static readonly Guid guidBundlerRunOnSaveCmdSet = new Guid(guidBundlerRunOnSaveCmdSetString);
+    };
+}
---
+++ src/Bundler2012/OutputWindowWriter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Bundler2012
+{
+    internal class OutputWindowWriter
+    {
+        private IVsOutputWindowPane _outputWindowPane;
+
+        
+        public OutputWindowWriter(IServiceProvider serviceProvider, string outWindowGuid, string outWindowName)
+        {
+            var outputWindow = serviceProvider.GetService(typeof(SVsOutputWindow)) as IVsOutputWindow;
+            if (outputWindow == null) throw new Exception("Unable to create an output pane.");
+
+            var paneGuid = new Guid(outWindowGuid);
+            outputWindow.GetPane(ref paneGuid, out _outputWindowPane);
+            if (_outputWindowPane == null)
+            {
+                outputWindow.CreatePane(ref paneGuid, outWindowName, 1, 0);
+                outputWindow.GetPane(ref paneGuid, out _outputWindowPane);
+            }
+        }
+
+        public void Write(string format, params object[] parameters)
+        {
+            if (_outputWindowPane == null || format == null) return;
+
+            _outputWindowPane.OutputString(String.Format(format, parameters));
+        }
+
+        public void WriteLine(string format, params object[] parameters)
+        {
+            Write(format + Environment.NewLine, parameters);
+        }
+
+        public void Clear()
+        {
+            _outputWindowPane.Clear();
+        }
+    }
+}
